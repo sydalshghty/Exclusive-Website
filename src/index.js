a93043fa0a1ebb0fc8be71b6c60c20826bf3fbba
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2025-03-08 22:48:53 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='Exclusive-Website'>
         <Provider store={store}>
             <App />
         </Provider>
